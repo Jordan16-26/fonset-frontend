5dcdbe22d8c7622f516cf7b296cac64a7b74e408
jordanglobal1626 <orozcojo@globalhitss.com>
2023-07-28 17:27:33 -0500

    Nuevos ajustes

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,22 +1,36 @@
-
 import React from "react";
 
 import "./layout.scss";
 import Header from "./header/index";
+import { Col, Container, Row } from "react-bootstrap";
+import SideBar from "./sidebar";
+import Footer from "./footer";
 
 // interface LayoutInterface {
 //     children?: React.ReactNode;
 // }
-const Layout: React.FC<any> = ({ children }) => {
-
-    return (
-        <main>
-            {/* <Header /> */}
-            <div className="content">
-                {children}
-            </div>
-        </main>
-    )
+interface LayoutInterface {
+  children?: React.ReactNode;
 }
+
+const Layout: React.FC<LayoutInterface> = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <Container fluid>
+        <Row>
+          <Col md={3} className="d-none d-md-block">
+            {/* Aquí se incluye el Sidebar */}
+            <SideBar />
+          </Col>
+          <Col md={9}>
+            <main>{children}</main>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
