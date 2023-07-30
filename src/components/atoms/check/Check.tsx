3f14a1303d19d89a90adb7a6b1ee91ffb323a49f
jordanglobal1626 <orozcojo@globalhitss.com>
2023-07-30 14:01:31 -0500

    nuevos ajustes

--- src/components/atoms/check/Check.tsx
+++ src/components/atoms/check/Check.tsx
@@ -3,16 +3,16 @@ import Form from 'react-bootstrap/Form';
 import { FormCheckType } from 'react-bootstrap/esm/FormCheck';
 
 interface CheckProps {
-  label: string;
-  type: FormCheckType;
+	label: string;
+	type: FormCheckType;
 }
 
-export default function CheckBox({label, type}: CheckProps) {
-  return (
-    <Form>
-        <div className="mb-3">
-          <Form.Check type={type} label={label} />
-        </div>
-    </Form>
-  );
+export default function CheckBox({ label, type }: CheckProps) {
+	return (
+		<Form>
+			<div className="mb-3">
+				<Form.Check type={type} label={label} />
+			</div>
+		</Form>
+	);
 }
